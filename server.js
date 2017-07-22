ed1440607a9ca400a13d2708d7200805068c22dd
Canmert <canmert.celikler@gmail.com>
2017-07-22 16:51:35 +0000

    working just as wanted

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 var express = require("express")
 var app = express()
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 app.get('/:time',function(req, res){
-    var time = req.params.time
-    res.send(time);
+    var time = req.params.time;
+    if(time.split(",").length == 1){
+        var timeParser = new Date(time*1000);
+        res.send(JSON.stringify({
+            unix : time,
+            natural : months[timeParser.getMonth()]+" "+timeParser.getDate()+", "+timeParser.getFullYear()
+        }))
+    }
+    else{
+        var timeParser = (new Date(time).getTime()/1000).toString();
+        res.send(JSON.stringify({
+            unix : timeParser,
+            natural : time
+        }))        
+    }
 })
 app.listen(process.env.PORT)
